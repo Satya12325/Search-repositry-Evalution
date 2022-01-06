aaa6f672b7f11056213b0b79f53ded81fee53715
Satyabrata Das <satyabrata_pw1_135@masai.school>
2022-01-06 14:40:37 +0530

    add pagination

--- src/Pages/Search.jsx
+++ src/Pages/Search.jsx
@@ -1,6 +1,6 @@
 import {useDispatch, useSelector} from "react-redux";
 import {getDataRepositry} from "../redux/app/action"
-import {useState} from "react";
+import {useState,useEffect} from "react";
 import styles from './Styles.module.css'
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
@@ -35,15 +35,31 @@ const useStyles = makeStyles((theme) => ({
 
 
 
+const Pagination = ({ totalPages, currentPage, onClickCallback }) => {
+  const pages = new Array(totalPages).fill(0).map((a, i) =>
+   currentPage === i + 1  ? (
+      <button disabled style={{ background: "olive" }} key={i}>
+        {i + 1}
+      </button>
+    ) : (
+      <button onClick={() => onClickCallback(i + 1)} key={i}>
+        {i + 1}
+      </button>
+    )
+  );
+  return <div style={{ display: "flex", gap: "1rem" , margin: "30px"}}>{pages}</div>;
+};
 
 
 
 function Search(){
-    
+  const [isLoading, setIsLoading] = useState(true);
     const [dato, setDato] = useState("");
+    const [page, setPage] = useState(1);
+    const [totalPages, setTotalPages] = useState(null)
     const dispatch =  useDispatch();
     const getData =  (dato) =>{
-        return fetch(`https://api.github.com/search/repositories?q=${dato}`)
+        return fetch(`https://api.github.com/search/repositories?q=${dato}&page=${page}`)
         .then(res => res.json())
         .then((res) => {
             const getData = getDataRepositry(res.items);
@@ -55,9 +71,21 @@ function Search(){
 
     const {data} = useSelector((state) => state.app)
     const handleClick = () => {
-        getData(dato)
+        getData(dato,page)
     }
+    useEffect(() =>{
+      handleClick(dato,page)
+    },[dato,page])
 
+    const changePage = (num) => {
+      if(num <= 1){
+        setPage(1);
+        return;
+      }
+      setPage(num)
+    };
+
+    const per_page = 4;
     const classes = useStyles();
 
   return (
@@ -77,7 +105,9 @@ function Search(){
      </div>
      <div className="App">
          {
-             data.map((item) => {
+           data?.filter(
+             (_,i) => i >=(page-1)*per_page && i < page * per_page
+           ).map((item) => {            
                  return <FullWidthGrid key={item.id}
                  id={item.id}
                  repo={item.description}
@@ -89,6 +119,13 @@ function Search(){
                 
              })
          }
+     </div>
+     <div className="App">
+       <Pagination
+       totalPages={Math.ceil(30/per_page)} 
+       currentPage={page}
+        onClickCallback={(page) =>changePage(page)}
+       />
      </div>
     </>
   );
